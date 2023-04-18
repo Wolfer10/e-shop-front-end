b64b4e415374b002e6c0893f2e6cf60d1d5b6b24
Wolf Ferenc <fwolf110@gmail.com>
2023-04-18 15:21:09 +0200

    creating the moduls, services and components

---
+++ src/app/bonus/bonus.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BonusComponent } from './bonus/bonus.component';
+
+
+
+@NgModule({
+  declarations: [
+    BonusComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BonusModule { }
