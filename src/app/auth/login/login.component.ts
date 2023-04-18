2c81d33dd9de8ac413307a152ebaad6b23e6f67c
Wolf Ferenc <fwolf110@gmail.com>
2023-04-18 21:22:57 +0200

    create login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup = new FormGroup('');
+
+  errorMessage: string = '';
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  get username() {
+    return this.loginForm.get('username')!;
+  }
+
+  get password() {
+    return this.loginForm.get('password')!;
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.authService.login({username, password}).subscribe({
+        next: next => {this.router.navigate(['/products']);},
+        error: err => this.errorMessage = err
+      });
+    }
+  }
 
 }
+
