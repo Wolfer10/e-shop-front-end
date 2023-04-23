4941854a182b8a3d12f273a6bb5cd6cb3fc9ae74
Wolf Ferenc <fwolf110@gmail.com>
2023-04-23 10:40:50 +0200

    add product detail  html

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Product} from "../../model/product";
+import {Shoe} from "../../model/shoe";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-detail',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class ProductDetailComponent {
 
+
+  product: Product = {
+    id: '',
+    name: '',
+    price: 0,
+    type: '',
+  }
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(){
+    this.fetchProduct();
+  }
+
+  fetchProduct() {
+    this.http.get<any>(`http://localhost:3000/products/${this.route.snapshot.params['id']}`).subscribe(data => {
+      this.product = new Shoe(data["_id"], data["name"], data["price"], data["type"]);
+    })
+  }
+
 }
+
