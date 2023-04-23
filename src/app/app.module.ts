7b0352f7dd83b9ec46a6fae14ff0d87993c15496
Wolf Ferenc <fwolf110@gmail.com>
2023-04-23 10:37:31 +0200

    appModule imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,39 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {AuthModule} from "./auth/auth.module";
 import { AppRoutingModule } from './app-routing.module';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {CommonModule} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
+import {NavButtonModule} from "./shared/nav-button/nav-button.module";
+import {HeaderModule} from "./shared/header/header.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
     AuthModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    NavButtonModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
