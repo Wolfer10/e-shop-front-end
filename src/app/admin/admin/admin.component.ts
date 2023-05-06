24ae7f994e20b8ca5eaeb3addca47fd57387acf8
Wolf Ferenc <fwolf110@gmail.com>
2023-05-06 12:14:30 +0200

    admin deleting button

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -11,14 +11,25 @@ import {AdminModule} from "../admin.module";
 })
 export class AdminComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'name', 'type', 'price'];
+  displayedColumns: string[] = ['id', 'name', 'type', 'price', 'delete'];
+
   dataSource: MatTableDataSource<Shoe> = new MatTableDataSource();
   constructor(private productService: ProductsService) {}
   ngOnInit() {
     this.productService.getShoes().subscribe(shoes => {
       this.dataSource = new MatTableDataSource(shoes);
     });
+  }
 
+  deleteShoe(shoeId: String) {
+    console.log(shoeId)
+    this.productService.deleteProduct(shoeId).subscribe(() => {
+      const index = this.dataSource.data.findIndex((shoe: Shoe) => shoe.id === shoeId);
+      if (index !== -1) {
+        this.dataSource.data.splice(index, 1);
+        this.dataSource._updateChangeSubscription();
+      }
+    });
   }
 
 }
